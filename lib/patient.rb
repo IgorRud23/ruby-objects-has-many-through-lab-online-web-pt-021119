9699175419ba1669c7c30961080151a4a6c8921b
Igor Rud <rud.igor23@gmail.com>
2019-03-22 23:18:01 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,12 +2,16 @@ class Patient
 
 attr_accessor :name, :appointment, :doctor
 
-
+@@all = []
 
 def initialize(name)
   @name = name
-
+@@all << self
 end
+
+def self.all
+@@all
+end 
 
 def new_appointment(doctor, date)
   Appointment.new(self, doctor, date)
