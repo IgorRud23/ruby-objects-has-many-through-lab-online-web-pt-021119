f8d1af8da8ffbb971da360ff8d19abb531c6cb72
Igor Rud <rud.igor23@gmail.com>
2019-03-22 22:51:17 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,24 @@
+class Patient
+
+attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def new_appointment(doctor, date)
+  Appointment.new(doctor, date, self)
+end
+
+def appointments
+Appointment.all
+end
+
+end
